b81e97a86bee9b849159678d732b63d6bbd210f4
Daroz <geraldodarosfilho@hotmail.com>
2022-01-11 00:56:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ProdutoServiceService} from "./produto-service.service";
+import {Produto} from "./models/Produto";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private produtoService: ProdutoServiceService) {
+  }
+
   title = 'supermercado-frontend';
+  produto:Produto[] = [];
+
+  ngOnInit(): void {
+    this.produtoService.pegarProdutos().subscribe(resposta => {
+        this.produto = resposta;
+    }, error => {
+
+    })
+  }
+
 }
---
+++ src/app/models/Produto.ts
@@ -0,0 +1,9 @@
+export class Produto {
+
+  id: number = 0;
+  descricao: string = '';
+  preco: number = 0;
+  quantidade: number = 0;
+  codigo: number = 0;
+
+}
